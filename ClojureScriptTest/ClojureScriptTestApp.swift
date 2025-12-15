7abf055ce92b9d084543c5e1b32a45fbf2fb8d7a
Rod Schmidt <rod@infinitenil.com>
2025-12-15 11:15:05 -0700

    Initial Commit

---
+++ ClojureScriptTest/ClojureScriptTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClojureScriptTestApp.swift
+//  ClojureScriptTest
+//
+//  Created by Rod Schmidt on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ClojureScriptTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
